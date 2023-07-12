85dd9dbfa6d7be424c04cdf0e901e49742b6db60
dcasUni <116928566+dcasUni@users.noreply.github.com>
2023-07-12 17:39:40 -0400

    Added class within class

--- Main.java
+++ Main.java
@@ -1,41 +1,20 @@
 import java.util.HashMap;
 import java.util.Scanner;
 
-public class Product {
-    private String name;
-    private String serial;
-    private int value;
-    
-    public Product(String name, String serial, int value) {
-        this.name = name;
-        this.serial = serial;
-        this.value = value;
-    }
-    
-    public String getName() {
-        return name;
-    }
-    
-    public String getSerial() {
-        return serial;
-    }
-    
-    public int getValue() {
-        return value;
-    }
-}
-
-class Main {
+public class InventorySystem {
+	public static Scanner scanner = new Scanner(System.in);
+	public static HashMap<String, Product> products = new HashMap<>();
+	
 	public static void main(String[] args) {
 		menu();
-    	}
+    }
 	
 	private static void menu() {
 		System.out.println("Press 1 to add an item.\n" + 
-				   "Press 2 to delete an item.\n" +
-				   "Press 3 to update an item.\n" + 
-				   "Press 4 to show all the items.\n" +
-				   "Press 5 to quit the program.");
+				   		   "Press 2 to delete an item.\n" +
+				   		   "Press 3 to update an item.\n" + 
+				   		   "Press 4 to show all the items.\n" +
+				   		   "Press 5 to quit the program.");
 		int choice = Integer.parseInt(scanner.nextLine());
 		choiceProcessor(choice);
 	}
@@ -66,7 +45,7 @@ class Main {
             System.out.println("Number is not 1, 2, 3, 4, or 5!");
             break;
     	}
-    	}
+    }
 	
 	private static void add() {
 		System.out.println("Enter the name:");
@@ -108,5 +87,28 @@ class Main {
 		}
 		menu();
 	}
+	
+	public static class Product {
+	    private String name;
+	    private String serial;
+	    private int value;
+	    
+	    public Product(String name, String serial, int value) {
+	        this.name = name;
+	        this.serial = serial;
+	        this.value = value;
+	    }
+	    
+	    public String getName() {
+	        return name;
+	    }
+	    
+	    public String getSerial() {
+	        return serial;
+	    }
+	    
+	    public int getValue() {
+	        return value;
+	    }
 	}
 }
